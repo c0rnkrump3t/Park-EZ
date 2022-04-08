086e752cd8d99c1d52ab29492050fc4bea32ae51
Daria Koroleva <dasha.koroleva499@gmail.com>
2022-04-08 00:30:42 -0400

    Fix github routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import AvailableParking from './components/AvailableParking';
 import Available from './components/Available';
 import Unavailable from './components/Unavailable';
@@ -10,19 +10,17 @@ import Permit from './components/Permit';
 
 function App() {
   return (
-    <Routes>
-  
-   <Route path='/available' element={[<AvailableParking/>]} />
-   <Route path='/s001' element={[<Available/>]} />
-   <Route path='/s002' element={[<Unavailable/>]} />
-   <Route path='/s003' element={[<Pay/>]} />
-   <Route path='/s004' element={[<Handicap/>]} />
-   <Route path='/s005' element={[<Permit/>]} />
-   
-   
 
-
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/available' element={[<AvailableParking />]} />
+        <Route path='/s001' element={[<Available />]} />
+        <Route path='/s002' element={[<Unavailable />]} />
+        <Route path='/s003' element={[<Pay />]} />
+        <Route path='/s004' element={[<Handicap />]} />
+        <Route path='/s005' element={[<Permit />]} />
+      </Routes>
+    </BrowserRouter>
 
 
   );
